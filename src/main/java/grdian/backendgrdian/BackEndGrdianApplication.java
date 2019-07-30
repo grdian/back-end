4f146a3fbcafe7df279b25e4aaf81e14a1bdb0b4
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-29 20:52:55 -0400

    Initial build with Gradle.

---
+++ src/main/java/grdian/backendgrdian/BackEndGrdianApplication.java
@@ -0,0 +1,13 @@
+package grdian.backendgrdian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGrdianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndGrdianApplication.class, args);
+	}
+
+}
