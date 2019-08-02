937447dbd27354937e67a560fbabd6c1f13be888
nelhag <lamais2006@yahoo.com>
2019-08-02 14:11:19 -0400

    added add user fuction to user contrller

--- src/main/java/grdian/backendgrdian/controllers/UserController.java
+++ src/main/java/grdian/backendgrdian/controllers/UserController.java
@@ -1,14 +1,24 @@
 package grdian.backendgrdian.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import grdian.backendgrdian.entities.User;
 import grdian.backendgrdian.repos.UserRepository;
+
 
 @CrossOrigin
 @RestController
@@ -31,5 +41,25 @@ public class UserController {
 	public User findOneUser (@PathVariable Long id) {
 		return userRepo.findById(id).get();
 	}
-
+	@PostMapping("/users")
+	public void createNewUser(@RequestBody String body,HttpServletResponse response) 
+		throws JSONException, IOException{
+		JSONObject json=(JSONObject) JSONParser.parseJSON(body);
+		System.out.println(body);
+			String firstName=json.getString("firstName");
+			String lastName=json.getString("lastName");
+			String imgURL=json.getString("imgURL");
+			String phoneNumber=json.getString("phoneNumber");
+			String emailAddress=json.getString("emailAddress");
+			String password=json.getString("password");
+		
+		userRepo.save(new User(firstName, lastName, imgURL, phoneNumber,emailAddress, password));
+		response.sendRedirect("/api/users");
+	}
+@DeleteMapping("/users/{id}")
+public void deleteUser(@PathVariable() Long id, HttpServletResponse response) 
+		throws  IOException{
+	userRepo.deleteById(id);
+	response.sendRedirect("/api/users");
+}
 }
