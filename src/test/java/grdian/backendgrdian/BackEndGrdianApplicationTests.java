81d892248ced8bea781ccebcbb47cfecdb0fcbcb
tyler-conner <tyler.conner@gmail.com>
2019-08-12 15:12:04 -0400

    twilio added

--- src/test/java/grdian/backendgrdian/BackEndGrdianApplicationTests.java
+++ src/test/java/grdian/backendgrdian/BackEndGrdianApplicationTests.java
@@ -2,15 +2,25 @@ package grdian.backendgrdian;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import grdian.backendgrdian.utility.SmsSender;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackEndGrdianApplicationTests {
 
+	@Autowired
+	SmsSender smsSender;
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void sendsSmsMessage() {
+		//Enter recipient Phone Number into argument below
+		smsSender.sendSms("");
+	}
 }
